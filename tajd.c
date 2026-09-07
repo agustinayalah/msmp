45a688185db374de7106b02c0eff9d81a53ac86d
Agustin <agustinayalaho@gmail.com>
2026-09-07 01:01:26 -0300

    Formateo programa Tajima's D

--- tajd.c
+++ tajd.c
@@ -16,7 +16,8 @@ double c2f(int, double, double, double);
 double e1f(double, double);
 double e2f(double, double, double);
 
-double tajd(int nsam, int segsites, double sumk) {
+double tajd(int nsam, int segsites, double sumk) 
+{
     double a1, a2, b1, b2, c1, c2, e1, e2;
 
     if (segsites == 0) return (0.0);
@@ -33,7 +34,8 @@ double tajd(int nsam, int segsites, double sumk) {
     return ((sumk - (segsites / a1)) / sqrt((e1 * segsites) + ((e2 * segsites) * (segsites - 1))));
 }
 
-double a1f(int nsam) {
+double a1f(int nsam) 
+{
     double a1;
     int i;
     a1 = 0.0;
@@ -43,7 +45,8 @@ double a1f(int nsam) {
     return (a1);
 }
 
-double a2f(int nsam) {
+double a2f(int nsam) 
+{
     double a2;
     int i;
     a2 = 0.0;
@@ -52,37 +55,43 @@ double a2f(int nsam) {
     return (a2);
 }
 
-double b1f(int nsam) {
+double b1f(int nsam) 
+{
     double b1;
     b1 = (nsam + 1.0) / (3.0 * (nsam - 1.0));
     return (b1);
 }
 
-double b2f(int nsam) {
+double b2f(int nsam) 
+{
     double b2;
     b2 = (2 * (nsam * nsam + nsam + 3.0)) / (9 * nsam * (nsam - 1));
     return (b2);
 }
 
-double e1f(double a1, double c1) {
+double e1f(double a1, double c1) 
+{
     double e1;
     e1 = c1 / a1;
     return (e1);
 }
 
-double e2f(double a1, double a2, double c2) {
+double e2f(double a1, double a2, double c2) 
+{
     double e2;
     e2 = c2 / ((a1 * a1) + a2);
     return (e2);
 }
 
-double c1f(double a1, double b1) {
+double c1f(double a1, double b1) 
+{
     double c1;
     c1 = b1 - (1 / a1);
     return (c1);
 }
 
-double c2f(int nsam, double a1, double a2, double b2) {
+double c2f(int nsam, double a1, double a2, double b2) 
+{
     double c2;
     c2 = b2 - ((nsam + 2) / (a1 * nsam)) + (a2 / (a1 * a1));
     return (c2);
